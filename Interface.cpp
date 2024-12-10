0cb396ab43ebcbd7cae6bb96439e260623007b44
yg-sir <egor_sov2005@mail.ru>
2024-12-10 21:21:56 +0300

    sls

--- Interface.cpp
+++ Interface.cpp
@@ -7,7 +7,9 @@
 #include "Connection_of_client.h"
 #include "Errors.h"
 #include "Logger.h"
+
 namespace po = boost::program_options;
+
 int Interface::comm_proc(int argc, const char** argv)
 {
     bool flag_b = false;
@@ -16,18 +18,18 @@ int Interface::comm_proc(int argc, const char** argv)
     int PORT;
     std::string logfile;
     std::string basefile;
-  
+
     try {
         po::options_description opts("Allowed options");
         opts.add_options()
         ("help,h", "Show help")
         ("basefile,b",
-         po::value<std::string>()->default_value("/home/stud/local_git/Kursa4/file4test/base.txt"),
-         "option is string(path to file with database)") 
-        ("logfile,l",                        
-         po::value<std::string>()->default_value("/home/stud/local_git/Kursa4/file4test/log.txt"), 
+         po::value<std::string>()->default_value("/etc/vcalc.conf"),
+         "option is string(path to file with database)")
+        ("logfile,l",
+         po::value<std::string>()->default_value("/var/log/vcalc.log"),
          "option is string(path to file with logs)")
-        ("PORT,p",                               
+        ("PORT,p",
          po::value<int>(&PORT)->default_value(33333),
          "option is int (PORT for server)");
 
@@ -40,15 +42,12 @@ int Interface::comm_proc(int argc, const char** argv)
         }
 
         if(vm.count("basefile")) {
-            if(vm["basefile"].as<std::string>() == "/home/stud/local_git/Kursa4/file4test/base.txt") {
+            if(vm["basefile"].as<std::string>() == "/etc/vcalc.conf") {
                 flag_b = true;
             }
             basefile = vm["basefile"].as<std::string>();
         }
         if(vm.count("logfile")) {
-            if(vm["logfile"].as<std::string>() == "/home/stud/local_git/Kursa4/file4test/log.txt") {
-                flag_l = true;
-            }
             logfile = vm["logfile"].as<std::string>();
         }
 
@@ -57,62 +56,64 @@ int Interface::comm_proc(int argc, const char** argv)
                 flag_p = true;
             }
             PORT = vm["PORT"].as<int>();
-    }
- 
-   	if(PORT < 1024 || PORT > 65535){
-    throw crit_err("Incorrect port");
-}
-	if(flag_b and flag_l and flag_p) {
-        std::cout << "Server started with default parameters.\nUse -h for help"<<std::endl;
         }
-    Logger l1(logfile);
-        if(logfile != "/home/stud/local_git/Kursa4/file4test/log.txt") {
-            l1.writelog("Path to logfile set value: "+logfile);
+
+        if(PORT < 1024 || PORT > 65535) {
+            throw crit_err("Incorrect port");
+        }
+        
+        if(flag_b && flag_l && flag_p) {
+            std::cout << "Server started with default parameters.\nUse -h for help" << std::endl;
+        }
+
+        Logger l1(logfile);
+        if(logfile != "/var/log/vcalc.log") {
+            l1.writelog("Path to logfile set value: " + logfile);
         } else {
             l1.writelog("Path to logfile set default value");
         }
-    if(basefile != "/home/stud/local_git/Kursa4/file4test/base.txt") {
-        l1.writelog("Path to basefile set value: " + basefile);
-    } else {
-        l1.writelog("Path to basefile set default value");
-    }
-    if(PORT != 33333) {
-        l1.writelog("Port set not default value");
-    } else {
-        l1.writelog("Port set default value");
-    }
-    Connector_to_base c1;
-    c1.connect_to_base(basefile);
-    l1.writelog("Connect to database success!");
-    l1.writelog("Server started");
-    Client_Communicate con;
-    #if _UNITTEST_!=1
-    con.connection(PORT,c1.get_data(),&l1);
-	#endif
-    }catch(crit_err& e){
-    	#if _UNITTEST_ != 1
-    	std::cerr << "error: " << e.what() << "\n";
+        if(basefile != "/etc/vcalc.conf") {
+            l1.writelog("Path to basefile set value: " + basefile);
+        } else {
+            l1.writelog("Path to basefile set default value");
+        }
+        if(PORT != 33333) {
+            l1.writelog("Port set not default value");
+        } else {
+            l1.writelog("Port set default value");
+        }
+        Connector_to_base c1;
+        c1.connect_to_base(basefile);
+        l1.writelog("Connect to database success!");
+        l1.writelog("Server started");
+        Client_Communicate con;
+#if _UNITTEST_!=1
+        con.connection(PORT, c1.get_data(), &l1);
+#endif
+    } catch (crit_err& e) {
+#if _UNITTEST_ != 1
+        std::cerr << "error: " << e.what() << "\n";
         std::cerr << "Критическая ошибка" << std::endl;
-        #endif
-        #if _UNITTEST_ ==1
+#endif
+#if _UNITTEST_ == 1
         std::cerr << "crit_err exception" << std::endl;
-        #endif
-    }catch(po::error& e) {
-    	#if _UNITTEST_ != 1
+#endif
+    } catch(po::error& e) {
+#if _UNITTEST_ != 1
         std::cerr << "error: " << e.what() << "\n";
         std::cerr << "Use -h for help\n";
-        #endif
-        #if _UNITTEST_ ==1
+#endif
+#if _UNITTEST_ == 1
         std::cerr << "Boost errors" << std::endl;
-        #endif
-    }catch(std::exception& e) {
-    	#if _UNITTEST_ != 1
+#endif
+    } catch(std::exception& e) {
+#if _UNITTEST_ != 1
         std::cerr << "error: " << e.what() << "\n";
         std::cerr << "Use -h for help\n";
-        #endif
-        #if _UNITTEST_ ==1
+#endif
+#if _UNITTEST_ == 1
         std::cerr << "Exception" << std::endl;
-        #endif
+#endif
     } catch(...) {
         std::cerr << "Exception of unknown type!\n";
         std::cerr << "Use -h for help\n";
--- Logger.cpp
+++ Logger.cpp
@@ -3,35 +3,57 @@
 #include <string>
 #include <time.h>
 #include <fstream>
+#include <iostream>  
 #include "Errors.h"
+
 using namespace std;
-int Logger::set_path(std::string path_file){
-	if(path_file.find('.') == std::string::npos){throw std::invalid_argument("file without extension");}
+
+int Logger::set_path(std::string path_file) {
+    if(path_file.find('.') == std::string::npos) {
+        throw std::invalid_argument("file without extension");
+    }
     std::ofstream filelog;
-    filelog.open(path_file,std::ios_base::out | std::ios_base::app);
+    filelog.open(path_file, std::ios_base::out | std::ios_base::app);
     if(filelog.is_open()) {
+        path_to_logfile = path_file;
         return 0;
-    }else{throw crit_err("path to log not exist");}}
-string Logger::getCurrentDateTime( string s ){
+    } else if (path_file == "/var/log/vcalc.log") {
+        path_to_logfile = "/tmp/log/vcalc.log";
+        filelog.open(path_to_logfile, std::ios_base::out | std::ios_base::app);
+        if (filelog.is_open()) {
+            return 0;
+        }
+    }
+    throw crit_err("path to log not exist");
+}
+
+string Logger::getCurrentDateTime(string s) {
     time_t now = time(0);
     struct tm  tstruct;
     char  buf[80];
     tstruct = *localtime(&now);
-    if(s=="now")
+    if(s == "now")
         strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
-    else if(s=="date")
+    else if(s == "date")
         strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
     return string(buf);
-};
-int Logger::writelog(std::string s)
-{
-    std::ofstream filelog;
-    filelog.open(path_to_logfile,std::ios_base::out | std::ios_base::app);
-    if(filelog.is_open()) {
-        std::string time = getCurrentDateTime("now");
-        filelog << time<<' '<< s << std::endl;
-    }else{
-		throw crit_err("path to log not exist");
 }
+
+int Logger::writelog(std::string s) {
+    std::ofstream filelog(path_to_logfile, std::ios_base::out | std::ios_base::app);
+
+    if (!filelog.is_open()) {
+        std::cerr << "Ошибка: не удалось создать лог в " << path_to_logfile
+                  << ". Переключение на /tmp/log/vcalc.log" << std::endl;
+        path_to_logfile = "/tmp/log/vcalc.log";
+        filelog.open(path_to_logfile, std::ios_base::out | std::ios_base::app);
+
+        if (!filelog.is_open()) {
+            throw crit_err("Не удалось создать лог-файл: " + path_to_logfile);
+        }
+    }
+
+    std::string time = getCurrentDateTime("now");
+    filelog << time << ' ' << s << std::endl;
     return 0;
 }
